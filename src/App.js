1be466f46fe7d2672b5eadbf830fb24de8fac526
Beth Johnson <beth@desmos.com>
2022-01-07 17:25:04 -0800

    squares change color when clicked

--- src/App.js
+++ src/App.js
@@ -17,20 +17,32 @@ const createInitialState = (num) => {
 
 function App() {
   const [grid, setGrid] = useState(createInitialState(GRID_SIZE));
+  const onSquareUpdate = (clickedRowIdx, clickedColIdx, newValue) => {
+    const newGrid = grid.map((row, rowIdx) => {
+      return row.map((value, colIdx) => {
+        if (clickedColIdx === colIdx && clickedRowIdx === rowIdx) {
+          return newValue;
+        } else {
+          return value;
+        }
+      })
+    });
+    setGrid(newGrid);
+  };
   return (
     <div className="grid">
-      { grid.map(row => <Row row={row} />)}
+      { grid.map((row, rowIdx) => <Row row={row} rowIdx={rowIdx} onSquareUpdate={onSquareUpdate} key={rowIdx}/>)}
     </div>
   );
 }
 
-function Row({row}) {
+function Row({row, rowIdx, onSquareUpdate}) {
   return <div className="row">
-    {row.map(square => <Square color={square}/>)}
+    {row.map((square, colIdx) => <Square color={square} rowIdx={rowIdx} colIdx={colIdx} onSquareUpdate={onSquareUpdate} key={colIdx}/>)}
   </div>
 }
 
-function Square({color}) {
+function Square({color, rowIdx, colIdx, onSquareUpdate}) {
   let classNameColor = '';
   if (color === GREEN) {
     classNameColor = 'green';
@@ -39,7 +51,11 @@ function Square({color}) {
   } else if (color === PURPLE) {
     classNameColor = 'purple';
   }
-  return (<div className={`square ${classNameColor}`}></div>)
+  const onClick = () => {
+    let newColor = (color + 1) % 3;
+    onSquareUpdate(rowIdx, colIdx, newColor);
+  }
+  return (<div className={`square ${classNameColor}`} onClick={onClick}></div>)
 }
 
 export default App;
